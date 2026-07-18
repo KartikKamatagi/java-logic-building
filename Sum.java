4e444eecb7951b3d30a8ba7a00f419035e0f22d3
KartikKamatagi <kartikkamatagi2004@gmail.com>
2026-07-18 10:20:14 +0530

    Sum of digits

---
+++ Sum.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number");
+        int n=sc.nextInt();
+        int sum=0;
+
+        while (n != 0) { 
+            int d=n%10;
+            sum=sum + d;
+            n=n/10;
+
+        }
+        System.out.println("Sum: "+sum);
+    }
+    
+}
