e81ec02e0f0f4baa80a4c1bbaac1212d7f776f5a
KartikKamatagi <kartikkamatagi2004@gmail.com>
2026-07-18 10:21:30 +0530

    Sum of digits

--- Sum.java
+++ Sum.java
@@ -7,7 +7,6 @@ public class Sum {
         System.out.println("Enter Number");
         int n=sc.nextInt();
         int sum=0;
-
         while (n != 0) { 
             int d=n%10;
             sum=sum + d;
