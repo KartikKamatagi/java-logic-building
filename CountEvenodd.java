31c60006151c6e08b413d528f04208804acd2a5e
KartikKamatagi <kartikkamatagi2004@gmail.com>
2026-07-19 18:37:55 +0530

    Sum of digits

---
+++ CountEvenodd.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class CountEvenodd {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter numbers");
+
+        int n=sc.nextInt();
+
+        int even=0;
+        int odd=0;
+        while (n != 0) { 
+            int d=n%10;
+
+            if(d%2==0){
+                even ++;
+
+            }
+            else{
+                odd ++;
+            }
+            n=n/10;
+        }
+        System.out.println("Count Even: " + even);
+        System.out.println("Count Odd: " + odd);
+    }
+}
