578d6b4965374ab3ebc421ab171fce309d49bf66
KartikKamatagi <kartikkamatagi2004@gmail.com>
2026-07-20 23:20:09 +0530

    Even and odd number

--- Even.java
+++ Even.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 public class Even {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter a number");
+        System.out.println("Enter a number");
+        
         int n=sc.nextInt();
 
         if(n%2==0){
