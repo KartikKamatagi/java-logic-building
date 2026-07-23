b40c6d61191e61cba3088c8cd01fd0961cb2f464
KartikKamatagi <kartikkamatagi2004@gmail.com>
2026-07-23 22:40:53 +0530

    Sum of Natural num

--- NaturalSum.java
+++ NaturalSum.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class NaturalSum {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter Number: ");
+        System.out.println("Enter a Number: ");
         int num=sc.nextInt();
 
         int sum=0;
