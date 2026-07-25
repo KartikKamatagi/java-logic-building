f5c1935a7ed6386f46e0f86c493eed2f9605a6b2
KartikKamatagi <kartikkamatagi2004@gmail.com>
2026-07-25 22:43:18 +0530

    Factorial

--- Factorial.java
+++ Factorial.java
@@ -4,10 +4,11 @@ public class Factorial {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         System.out.println("");
+        System.out.println("enter a number: ");
         int num=sc.nextInt();
-        int fact=0;
+        int fact=1;
 
-        for(int i=1; i<num; i++){
+        for(int i=1; i<=num; i++){
 
             fact=fact*i;
             
