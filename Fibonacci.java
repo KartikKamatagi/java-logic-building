ddfd4bd97a03f11dafc485ed86b0546cb7c680d6
KartikKamatagi <kartikkamatagi2004@gmail.com>
2026-07-27 23:04:10 +0530

    fibonacci

---
+++ Fibonacci.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter terms of number: ");
+        int n=sc.nextInt();
+
+        int a=0;
+        int b=1;
+
+        System.out.println("Enter Series: ");
+        for(int i=0)
+    }
+}
