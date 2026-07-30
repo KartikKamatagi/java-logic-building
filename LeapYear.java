c3139636edd7037939afc5f52f143f76e04cf416
KartikKamatagi <kartikkamatagi2004@gmail.com>
2026-07-30 22:31:29 +0530

    leap year

--- LeapYear.java
+++ LeapYear.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 public class LeapYear {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter A year");
+        System.out.println("Enter a year: ");
         int year=sc.nextInt();
 
         if(year % 400 == 0|| (year % 4==0 && year % 100 !=0)){
-            System.out.println("Its a leaf yaer");
+            System.out.println("It's a leap year");
         }
         else{
-            System.out.println("its not leap yaer");
+            System.out.println("it's not leap year");
         }
     }
     
