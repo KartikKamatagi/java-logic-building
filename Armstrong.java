fea9d089d151a6dafdeb8f1f38d33b9b626c6fe2
KartikKamatagi <kartikkamatagi2004@gmail.com>
2026-08-02 23:32:53 +0530

    Armstrong Number

---
+++ Armstrong.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+class Armstrong{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter A number");
+        int n=sc.nextInt();
+
+        int original=n;
+        int sum=0;
+
+        while(n != 0){
+            int digit=n % 10;
+            sum=sum+(digit * digit *digit);
+            n=n/10;
+        }
+        if(sum == original){
+            System.out.println("Armstrong ");
+        }else{
+            System.out.println("not");
+        }
+    }
+
+}
