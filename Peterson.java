22986bb7fb5b154d9a9e0f8cf727ca202aa46b78
KartikKamatagi <kartikkamatagi2004@gmail.com>
2026-08-05 23:43:11 +0530

    Peterson Number

---
+++ Peterson.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class Peterson {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+
+        int num=sc.nextInt();
+
+        int original=num;
+        int sum=0;
+
+        while(num != 0){
+            int digit=num % 10;
+
+            int fact=1;
+
+            for(int i=1; i<=digit; i++){
+                fact= fact*i;
+            }
+            sum=sum + fact;
+            num=num/10;
+        }
+        if(sum == original){
+            System.out.println("Its a peterson");
+        }
+        else{
+            System.out.println("Its not peterson");
+        }
+    }
+}
