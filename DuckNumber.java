858a831b6639676c2458e4f42cb1a3b9bc6db1af
KartikKamatagi <kartikkamatagi2004@gmail.com>
2026-08-09 22:20:59 +0530

    Duck Number

---
+++ DuckNumber.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class DuckNumber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        boolean duck = false;
+
+        while (num != 0) {
+
+            int digit = num % 10;
+
+            if (digit == 0) {
+                duck = true;
+                break;
+            }
+
+            num = num / 10;
+        }
+
+        if (duck) {
+            System.out.println("Duck Number");
+        } else {
+            System.out.println("Not a Duck Number");
+        }
+
+        sc.close();
+    }
+}
