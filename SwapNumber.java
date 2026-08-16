268720be2c019a8e42523c541493cfbfe11d76fe
KartikKamatagi <kartikkamatagi2004@gmail.com>
2026-08-16 19:23:13 +0530

    Swap Numbers

---
+++ SwapNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter b: ");
+        int b = sc.nextInt();
+
+        int temp;
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        sc.close();
+    }
+}
