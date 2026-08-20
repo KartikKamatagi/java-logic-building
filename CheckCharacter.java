96071192fdfcedf07f071c453a96aeb1372bb7da
KartikKamatagi <kartikkamatagi2004@gmail.com>
2026-08-20 22:03:50 +0530

    CheckCharacter

--- CheckCharacter.java
+++ CheckCharacter.java
@@ -11,10 +11,11 @@ public class CheckCharacter {
         if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
             System.out.println("Alphabet");
         } 
-        
+
         else if (ch >= '0' && ch <= '9') {
             System.out.println("Digit");
         } 
+        
         else {
             System.out.println("Special Character");
         }
