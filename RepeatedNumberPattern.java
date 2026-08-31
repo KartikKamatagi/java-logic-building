e57037e553b22cd9e5009482c2c3aad86514502e
KartikKamatagi <kartikkamatagi2004@gmail.com>
2026-08-31 21:49:47 +0530

    RepeatedNumberPattern

--- RepeatedNumberPattern.java
+++ RepeatedNumberPattern.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class RepeatedNumberPattern {
     public static void main(String[] args) {
 
+
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter number of rows: ");
@@ -11,6 +12,7 @@ public class RepeatedNumberPattern {
         for (int i = 1; i <= n; i++) {
 
             for (int j = 1; j <= i; j++) {
+
                 System.out.print(i);
             }
 
