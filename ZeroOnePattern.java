2db1b5c0d98df352ff6e9080ecf470f0c3081be7
KartikKamatagi <kartikkamatagi2004@gmail.com>
2026-09-02 21:59:46 +0530

    ZeroOnePattern

--- ZeroOnePattern.java
+++ ZeroOnePattern.java
@@ -11,6 +11,7 @@ public class ZeroOnePattern {
         for (int i = 1; i <= n; i++) {
 
             for (int j = 1; j <= i; j++) {
+                
 
                 if ((i + j) % 2 == 0) {
                     System.out.print("1");
