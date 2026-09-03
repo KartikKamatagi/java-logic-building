f3fb8858ce290a0ab59bf9de0fffc2fa5f5ad3ae
KartikKamatagi <kartikkamatagi2004@gmail.com>
2026-09-03 22:35:03 +0530

    New Pattern

--- NewPattern.java
+++ NewPattern.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 class NewPattern{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter Number: ");
+        System.out.println("Enter Pattern Number: ");
         int n=sc.nextInt();
 
         for(int i=1; i<=n; i++){
