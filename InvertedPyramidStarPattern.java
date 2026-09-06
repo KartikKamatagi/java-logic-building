9877bc112e5ee3d143eba463279db6c5d9573491
KartikKamatagi <kartikkamatagi2004@gmail.com>
2026-09-06 22:12:48 +0530

    PyramidStarPattern

---
+++ InvertedPyramidStarPattern.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class InvertedPyramidStarPattern {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of pattern");
+        int n=sc.nextInt();
+
+       for (int i = n; i >= 1; i--) {
+
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+
+    }
+}
