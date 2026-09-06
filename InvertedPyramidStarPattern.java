e7de5a70f5ab8221b863b786ddba866b2acc3f8a
KartikKamatagi <kartikkamatagi2004@gmail.com>
2026-09-06 22:13:11 +0530

    PyramidStarPattern

--- InvertedPyramidStarPattern.java
+++ InvertedPyramidStarPattern.java
@@ -9,12 +9,12 @@ public class InvertedPyramidStarPattern {
 
        for (int i = n; i >= 1; i--) {
 
-            // Print spaces
+           
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
-            // Print stars
+            
             for (int j = 1; j <= 2 * i - 1; j++) {
                 System.out.print("*");
             }
