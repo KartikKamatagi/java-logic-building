478b4c63a5d8be512e18e3a62249eb3e1791c9b0
KartikKamatagi <kartikkamatagi2004@gmail.com>
2026-09-08 23:06:36 +0530

    FullInvertedPyramidPattern

--- FullInvertedPyramidPattern.java
+++ FullInvertedPyramidPattern.java
@@ -23,6 +23,7 @@ public class FullInvertedPyramidPattern {
                 System.out.print(" ");
             }
             for(int j=1 ; j<=2*i-1; j++){
+                
                 System.out.print("*");
             }
             
