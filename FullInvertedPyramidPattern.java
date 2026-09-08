42ffb1c8ba7affde59faa3212168ee761fa20116
KartikKamatagi <kartikkamatagi2004@gmail.com>
2026-09-08 23:11:00 +0530

    FullInvertedPyramidPattern

--- FullInvertedPyramidPattern.java
+++ FullInvertedPyramidPattern.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class FullInvertedPyramidPattern {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter number: ");
+        System.out.println("Enter Number: ");
         int n=sc.nextInt();
         for(int i=0; i<=n; i++){
 
